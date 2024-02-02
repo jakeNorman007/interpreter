91d41d13b6cac6b4f9f041c63a0f84c4e70ef171
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-02-01 22:30:41 -0500

    prefix expressions can be parsed now

--- ast/ast.go
+++ ast/ast.go
@@ -135,9 +135,24 @@ func (il *IntegerLiteral) expressionNode(){}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
 
+//Prefix Expressions
+type PrefixExpression struct {
+    Token       token.Token //prefix token, such as ! or -
+    Operator    string
+    Right       Expression
+}
 
+func (pe *PrefixExpression) expressionNode(){}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string { 
+    var out bytes.Buffer
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
 
-
+    return out.String()
+}
 
 
 
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,9 +1,11 @@
 package parser
 
 import (
-    "testing"
-    "github.com/JakeNorman007/interpreter/ast"
-    "github.com/JakeNorman007/interpreter/lexer"
+	"fmt"
+	"testing"
+
+	"github.com/JakeNorman007/interpreter/ast"
+	"github.com/JakeNorman007/interpreter/lexer"
 )
 
 //parser test for let statement currently
@@ -178,34 +180,66 @@ func TestIntegerLiteral(t *testing.T) {
     }
 }
 
+//parser test for prefix expressions - or !
+func TestParsingPrefixExpressions(t *testing.T) {
+    prefixTests := []struct {
+        input           string
+        operator        string
+        integerValue    int64
+    }{
+        {"!5;", "!", 5},
+        {"-15", "-", 15},
+    }
 
+    for _, tt := range prefixTests {
+        l := lexer.New(tt.input)
+        p := New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
 
+        if len(program.Statements) != 1 {
+            t.Fatalf("program does not contain %d statements, got=%d", 1, len(program.Statements))
+        }
 
+        stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+        if !ok {
+            t.Fatalf("program.Statement[0] is not ast.ExpressionStatement, got=%T", program.Statements[0])
+        }
 
+        exp, ok := stmt.Expression.(*ast.PrefixExpression)
+        if !ok {
+            t.Fatalf("stmt in not ast.PrefixExpression, got=%T", stmt.Expression)
+        }
+        
+        if exp.Operator != tt.operator {
+            t.Fatalf("exp.Operator is not %s, got=%s", tt.operator, exp.Operator)
+        }
 
+        if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
+            return
+        }
+    }
+}
 
+//testIntegerLiteral helper function to use with PrefixExpression test
+func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
+    integ, ok := il.(*ast.IntegerLiteral)
+    if !ok {
+        t.Errorf("il not *ast.IntegerLiteral, got=%T", il)
+        return false
+    }
 
+    if integ.Value != value {
+        t.Errorf("integ.Value is not %d, got=%d", value, integ.Value)
+        return false
+    }
 
+    if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
+        t.Errorf("integ.TokenLiteral is not %d, got=%s", value, integ.TokenLiteral())
+        return false
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return true
+}
 
 
