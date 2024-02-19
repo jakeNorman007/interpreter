466c030c863b6983d150f277bab5a459c32c161b
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-02-18 20:42:08 -0500

    ifs and blocks

--- ast/ast.go
+++ ast/ast.go
@@ -185,8 +185,20 @@ func (b *Boolean) expressionNode(){}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string { return b.Token.Literal }
 
+type IfExpression struct {
+    Token           token.Token
+    Condition       Expression
+    Consequence     *BlockStatement
+    Alternative     *BlockStatement
+}
 
+//here goes ti if expression instances
 
+type BlockStatement struct {
+    Token           token.Token
+    Statements      []Statement
+}
 
+//here will go the block statement instances
 
 
