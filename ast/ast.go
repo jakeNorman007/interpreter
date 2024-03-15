58fbd4218eb6c8a1a6c8d64be24b5a92d364366f
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-03-15 16:29:38 -0400

    function parsing

--- ast/ast.go
+++ ast/ast.go
@@ -1,8 +1,9 @@
 package ast
 
 import (
-    "bytes"
-    "github.com/JakeNorman007/interpreter/token"
+	"bytes"
+	"strings"
+	"github.com/JakeNorman007/interpreter/token"
 )
 
 //***********************AST Summary************************************
@@ -219,7 +220,7 @@ type BlockStatement struct {
 
 func (bs *BlockStatement) expressionNode(){}
 func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal}
-func (bs *BlockStatement) String() string{
+func (bs *BlockStatement) String() string {
     var out bytes.Buffer
 
     for _, s := range bs.Statements {
@@ -228,4 +229,48 @@ func (bs *BlockStatement) String() string{
 
     return out.String()
 }
+
+//funtion literals
+type FunctionLiteral struct {
+    Token       token.Token //this is the func token specifically
+    Parameters  []*Identifier
+    Body        *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode(){}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+    var out bytes.Buffer
+
+    params := []string{}
+    for _, p := range fl.Parameters {
+        params = append(params, p.String())
+    }
+
+    out.WriteString(fl.TokenLiteral())
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") ")
+    out.WriteString(fl.Body.String())
+
+    return out.String()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
