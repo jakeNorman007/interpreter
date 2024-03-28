be51fca54af9f2da46e1f36b567a2e0cab660b97
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-03-28 11:20:35 -0400

    .

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "io"
     "github.com/JakeNorman007/interpreter/lexer"
-    "github.com/JakeNorman007/interpreter/token"
+    "github.com/JakeNorman007/interpreter/parser"
 )
 
 const PROMPT = "[El]>> "
@@ -22,10 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
         line := scanner.Text()
         l := lexer.New(line)
+        p := parser.New(l)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok)
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParseErrors(out, p.Errors())
+            continue
         }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
     }
 
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+    for _, msg := range errors {
+        io.WriteString(out, "\t" + msg + "\n")
+    }
 }
